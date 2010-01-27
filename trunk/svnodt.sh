692f8b3c8538a685406b15a2f82b03155bcfa8ee
narendra.sisodiya <narendra.sisodiya@b408c158-f51c-11de-99d0-77da0b7ae2c6>
2010-01-27 21:23:41 +0000

    Deleted some files

--- trunk/svnodt.sh
+++ trunk/svnodt.sh
@@ -2,8 +2,8 @@
 
 
 ###      Put update on SVN   -- save changes
-unzip -u book.odt  # This will upzip 
-svn add ./*
+unzip -u book.odt  -d ./booksrc # This will upzip , Your book.odt into new files
+svn add ./*        # We need to add any new files before updating. This will add all files so do not put anything init.
 svn ci
 
 ###    Get update from SVN   -- get changes
