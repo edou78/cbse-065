d1970b64fe90147707d560d90f27316df6a573e6
narendra.sisodiya <narendra.sisodiya@b408c158-f51c-11de-99d0-77da0b7ae2c6>
2010-01-27 21:18:19 +0000

    Test

--- trunk/svnodt.sh
+++ trunk/svnodt.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 
+###      Put update on SVN   -- save changes
+unzip -u book.odt  # This will upzip 
+svn add ./*
+svn ci
 
+###    Get update from SVN   -- get changes
+svn update
+zip -vr bookold.odt * -x "*.svn*" "*.odt"
 
 exit 0
